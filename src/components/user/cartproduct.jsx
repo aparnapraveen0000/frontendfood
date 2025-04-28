62fde6fe698c20d5ed8a5eac95b4c4adc2a8629a
Aparna E H <aparnapraveen0000@gmail.com>
2025-04-28 23:52:30 +0530

    cart

--- src/components/user/cartproduct.jsx
+++ src/components/user/cartproduct.jsx
@@ -44,7 +44,7 @@ const Product = ({ product }) => {
         onClick={handleAddToCart}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       >
-        Add to Cart
+        cart
       </button>
     </div>
   );
